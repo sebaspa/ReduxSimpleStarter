1fbcb63c690183dd4482be589f7384e96dfa07fa
Javier Garcia <javiergarcia@jgarcia-m.local>
2017-12-05 10:55:21 -0500

    Importando la libreria para las consultas de youtube

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
  
 import SearchBar from './components/search_bar';
  
 const API_KEY = 'AIzaSyALKZGCOfeqe_aiEZ8JVV91g4U3HM3XlNI';
- 
-//Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        )
+    };
 }
- 
-//Take this component generated html and put it on the page (in DOM)
  
 ReactDOM.render(<App/>, document.querySelector('.container'));
